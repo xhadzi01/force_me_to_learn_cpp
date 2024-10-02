8b89a877d513abc849b5ba09b9bb3f419267f644
xhadzi01 <killjeaden52@gmail.com>
2024-10-02 22:04:54 +0200

    initial build file

---
+++ build.cmd
@@ -0,0 +1,24 @@
+@echo off
+set "CURRENT_SCRIPT_DIR=%~dp0"
+set "BUILD_FOLDER=%CURRENT_SCRIPT_DIR%/build"
+set "SRC_FOLDER=%CURRENT_SCRIPT_DIR%/src"
+set "CONFIG=Debug"
+
+if exist "%BUILD_FOLDER%" (
+	echo Recreating build folder
+	rmdir /S /Q "%BUILD_FOLDER%"
+	mkdir "%BUILD_FOLDER%"
+)
+
+
+PUSHD "%BUILD_FOLDER%"
+
+echo Building project in configuration %CONFIG%...
+cmake -S "%SRC_FOLDER%" -B "%BUILD_FOLDER%"
+cmake --build "%SRC_FOLDER%" --config %CONFIG%
+
+POPD
+
+
+@echo on
+exit /B 0
